d5bfe044e4e275893306571a9e7148fd05c002e3
kannokanno <akapanna@gmail.com>
2019-11-21 00:19:13 +0900

    登録UI

--- docs/mock1/bookshelf.js
+++ docs/mock1/bookshelf.js
@@ -6,6 +6,21 @@ new Vue({
     user: null,
     bookshelf: null,
     statistics: null,
+
+    showSearchSection: false,
+    showSearchResult: false,
+    addIndex: 0,
+    searchResults: [
+      { cover_url: 'https://cover.openbd.jp/9784000010870.jpg', link_count: 2, title: '', author: '' },
+      { cover_url: 'https://cover.openbd.jp/9784000241793.jpg', link_count: 1, title: '', author: '' },
+      { cover_url: 'https://cover.openbd.jp/9784000245395.jpg', link_count: 1, title: '', author: '' },
+      { cover_url: 'https://cover.openbd.jp/9784000613668.jpg', link_count: 4, title: '', author: '' },
+      { cover_url: 'https://cover.openbd.jp/9784000613675.jpg', link_count: 3, title: '', author: '' },
+      { cover_url: 'https://cover.openbd.jp/9784000613705.jpg', link_count: 2, title: '', author: '' },
+      { cover_url: 'https://cover.openbd.jp/9784001126860.jpg', link_count: 4, title: '', author: '' },
+      { cover_url: 'https://cover.openbd.jp/9784098604111.jpg', link_count: 0, title: '', author: '' },
+      { cover_url: 'https://cover.openbd.jp/9784098604425.jpg', link_count: 1, title: '', author: '' },
+    ],
   },
   created: function() {
     this.initData = createInitData();
@@ -18,10 +33,24 @@ new Vue({
       this.statistics = data.statistics;
     },
     showRandomBookshelf: function() {
-      var index = Math.floor((this.index + 1) % 3);
-      this.index = index;
-      this.updateData(this.initData[index]);
-    }
+      var nextIndex = this.index + 1;
+      if(nextIndex > 4) {
+        nextIndex = 1;
+      }
+      this.index = nextIndex;
+      this.updateData(this.initData[this.index]);
+    },
+
+
+    showAddForm: function(addIndex) {
+      this.addIndex = addIndex;
+      this.showSearchSection = true;
+    },
+    addBook: function(book) {
+      this.bookshelf.books[this.addIndex] = book;
+      this.showSearchSection = false;
+      this.showSearchResult = false;
+    },
   },
   computed: {
   }
--- docs/mock1/data.js
+++ docs/mock1/data.js
@@ -3,25 +3,27 @@ function createInitData() {
     _data1(),
     _data2(),
     _data3(),
+    _data4(),
+    _data5(),
   ];
 }
 
 function  _data1() {
   return {
-    user: { name: '本好き太郎', avatar_url: './data/1/avatar.png' },
-    statistics: { like: 214, follow: 34, follower: 300 },
+    user: { name: 'かんの', avatar_url: './data/1/avatar.png' },
+    statistics: { like: 0, follow: 0, follower: 0 },
     bookshelf: {
       name: '積読ベスト9',
       books: [
-        { cover_url: 'https://cover.openbd.jp/9784000010870.jpg', link_count: 2 },
-        { cover_url: 'https://cover.openbd.jp/9784000241793.jpg', link_count: 1 },
-        { cover_url: 'https://cover.openbd.jp/9784000245395.jpg', link_count: 1 },
-        { cover_url: 'https://cover.openbd.jp/9784000613668.jpg', link_count: 4 },
-        { cover_url: 'https://cover.openbd.jp/9784000613675.jpg', link_count: 3 },
-        { cover_url: 'https://cover.openbd.jp/9784000613705.jpg', link_count: 2 },
-        { cover_url: 'https://cover.openbd.jp/9784001126860.jpg', link_count: 4 },
-        { cover_url: 'https://cover.openbd.jp/9784098604111.jpg', link_count: 0 },
-        { cover_url: 'https://cover.openbd.jp/9784098604425.jpg', link_count: 1 },
+        { cover_url: null, link_count: 0 },
+        { cover_url: null, link_count: 0 },
+        { cover_url: null, link_count: 0 },
+        { cover_url: null, link_count: 0 },
+        { cover_url: null, link_count: 0 },
+        { cover_url: null, link_count: 0 },
+        { cover_url: null, link_count: 0 },
+        { cover_url: null, link_count: 0 },
+        { cover_url: null, link_count: 0 },
       ]
     }
   };
@@ -66,4 +68,46 @@ function  _data3() {
     }
   };
 }
+function  _data4() {
+  return {
+    user: { name: 'Something', avatar_url: './data/2/avatar.jpg' },
+    statistics: { like: 124, follow: 0, follower: 400 },
+    bookshelf: {
+      name: '',
+      books: [
+        { cover_url: 'https://cover.openbd.jp/9784098604111.jpg', link_count: 0 },
+        { cover_url: 'https://cover.openbd.jp/9784098604425.jpg', link_count: 2 },
+        { cover_url: 'https://cover.openbd.jp/9784098604432.jpg', link_count: 4 },
+        { cover_url: 'https://cover.openbd.jp/9784098604449.jpg', link_count: 1 },
+        { cover_url: 'https://cover.openbd.jp/9784098604463.jpg', link_count: 4 },
+        { cover_url: 'https://cover.openbd.jp/9784098604494.jpg', link_count: 1 },
+        { cover_url: 'https://cover.openbd.jp/9784098604500.jpg', link_count: 4 },
+        { cover_url: 'https://cover.openbd.jp/9784098604517.jpg', link_count: 1 },
+        { cover_url: 'https://cover.openbd.jp/9784098604524.jpg', link_count: 1 },
+      ]
+    }
+  };
+}
+function  _data5() {
+  return {
+    user: { name: 'ボブ', avatar_url: './data/3/avatar.jpg' },
+    statistics: { like: 124, follow: 0, follower: 400 },
+    bookshelf: {
+      name: '',
+      books: [
+        { cover_url: 'https://cover.openbd.jp/9784101015811.jpg', link_count: 4 },
+        { cover_url: 'https://cover.openbd.jp/9784101015910.jpg', link_count: 2 },
+        { cover_url: 'https://cover.openbd.jp/9784101016412.jpg', link_count: 4 },
+        { cover_url: 'https://cover.openbd.jp/9784101016511.jpg', link_count: 3 },
+        { cover_url: 'https://cover.openbd.jp/9784101192291.jpg', link_count: 1 },
+        { cover_url: 'https://cover.openbd.jp/9784101204574.jpg', link_count: 0 },
+        { cover_url: 'https://cover.openbd.jp/9784101223292.jpg', link_count: 4 },
+        { cover_url: 'https://cover.openbd.jp/9784101278148.jpg', link_count: 0 },
+        { cover_url: 'https://cover.openbd.jp/9784101390246.jpg', link_count: 2 },
+      ]
+    }
+  };
+}
+
+
 
